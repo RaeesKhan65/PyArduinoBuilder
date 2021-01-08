c799a19b4e56cffbf218786c8e06d6dae9a78b9b
RaeesKhan65 <65371159+RaeesKhan65@users.noreply.github.com>
2021-01-07 19:20:37 -0500

    commit

--- PyArdConn.py
+++ PyArdConn.py
@@ -2,12 +2,34 @@ import subprocess
 from pathlib import Path
 
 
-def newsketch(name):
-    try:
-        path = Path.joinpath(Path().resolve(),"ArduinoSketches",name)
-        subprocess.run(["arduino-cli","sketch","new",path])
-    except:
-        print("Error")
+class PyArduinoConn:
+
+    def __init__(self,name):
+        self.path = Path.joinpath(Path().resolve(),"ArduinoSketches",name)
+        self.core = "arduino:sam:arduino_due_x_dbg"
+        self.board = "/dev/cu.usbmodem14201"
 
 
-newsketch("test")
+    def newSketch(self):
+
+        try:
+            subprocess.run(["arduino-cli","sketch","new",self.path,"-v"])
+        except:
+            print("Error")
+
+
+    def compileSketch(self):
+
+        try:
+            subprocess.run(["arduino-cli","compile","-b",self.core,self.path,"-v"])
+        except:
+            print("Error")
+
+    def uploadSketch(self):
+
+        try:
+            subprocess.run(["arduino-cli","upload","-b",self.core,"-p",self.board,self.path,"-v"])
+        except:
+            print("Error")
+
+PyArduinoConn("test").newSketch()
